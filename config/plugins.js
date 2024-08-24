94ff60fe24c9175cd910ee8e2af68d303d41266b
liushooter <shooter.hero@gmail.com>
2024-08-25 02:22:30 +0800

    swagger config

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,36 @@
-module.exports = () => ({});
+// https://docs.strapi.io/dev-docs/plugins/documentation
+
+module.exports = {
+  documentation: {
+    enabled: true,
+    config: {
+      openapi: '3.0.0',
+      info: {
+        version: 'v1',
+        title: 'Strapi Doc',
+        description: '',
+        termsOfService: '',
+        contact: {
+          name: '',
+          email: '',
+          url: ''
+        },
+        license: {
+          name: '',
+          url: ''
+        },
+      },
+      'x-strapi-config': {
+        // Leave empty to ignore plugins during generation
+        plugins: [ 'upload', 'users-permissions'],
+        path: '/documentation',
+      },
+      servers: [{ url: 'http://localhost:1337/api', description: '' }],
+      externalDocs: {
+        url: '',
+        description: '',
+      },
+      security: [ { bearerAuth: [] } ]
+    }
+  }
+}
